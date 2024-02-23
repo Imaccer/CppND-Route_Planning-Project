462a7c2fc0261d8bc30537c95ff18d3458147990
Imaccer <mcnallyian@hotmail.com>
2024-02-23 11:28:56 -0800

    Added user inputs for start and end coords

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "route_planner.h"
 
 using namespace std::experimental;
+using std::cin;
+using std::cout;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
@@ -55,12 +57,34 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+     float start_x, start_y, end_x, end_y;
 
+    // Prompt the user for input
+    cout << "Enter start_x: ";
+    cin >> start_x;
+
+    cout << "Enter start_y: ";
+    cin >> start_y;
+
+    cout << "Enter end_x: ";
+    cin >> end_x;
+
+    cout << "Enter end_y: ";
+    cin >> end_y;
+
+    // Display the entered values
+    cout << "Entered values:" << "\n";
+    cout << "start_x: " << start_x << "\n";
+    cout << "start_y: " << start_y << "\n";
+    cout << "end_x: " << end_x << "\n";
+    cout << "end_y: " << end_y << "\n";
+     
+    
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
